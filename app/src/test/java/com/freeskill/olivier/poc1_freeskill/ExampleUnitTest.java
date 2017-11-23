4c4f652a83bb46b9b33fe54f631197490c6630e6
OlivierFaidherbe <olivier.faidherbe@isen.yncrea.fr>
2017-11-23 12:43:12 +0100

    Olivier POC V1

---
+++ app/src/test/java/com/freeskill/olivier/poc1_freeskill/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.freeskill.olivier.poc1_freeskill;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
